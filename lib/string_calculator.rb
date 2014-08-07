8d53fae5146b8c6938d5a552ddef31296c0c0ec7
Nestor Castillo <nestor.castillo@izea.com>
2014-08-06 20:53:07 -0400

    progress from dojo

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,1 +1,7 @@
-# define your class here
+class StringCalculator
+  def add(string_number)
+    return 0 if string_number.empty?
+
+    string_number.split(",").map{|string_num| string_num.to_i }.inject(0,:+)
+  end
+end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,5 +1,21 @@
 require "./lib/string_calculator"
 
-describe "Your test" do
+describe StringCalculator do
 
+  let (:string_calculator) { StringCalculator.new }
+  it "should return 0 for empty string" do
+     expect(string_calculator.add("")).to eq 0
+  end
+
+  it "should return 1 for 1" do
+    expect(string_calculator.add("1")).to eq 1
+  end
+
+  it "should add two numbers" do
+    expect(string_calculator.add("1,2")).to eq 3
+  end
+
+  it "should add five numbers" do
+    expect(string_calculator.add("1,2,3,4,54")).to eq 64
+  end
 end
